dc1fc8d87ea83e64b7a1759f2c88c7689f51e37c
zildirayalfirli <zildirayalfirli@gmail.com>
2023-12-12 09:37:45 +0700

    first commit

---
+++ responsi 2/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace responsi_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            panel1 = new System.Windows.Forms.Panel();
+            label9 = new System.Windows.Forms.Label();
+            label8 = new System.Windows.Forms.Label();
+            label7 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            dataGridView1 = new System.Windows.Forms.DataGridView();
+            btnInsert = new System.Windows.Forms.Button();
+            btnEdit = new System.Windows.Forms.Button();
+            btn_Delete = new System.Windows.Forms.Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(29, 53);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(71, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Logo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(29, 163);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(91, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Dep. Karyawan :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(29, 108);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(99, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Nama Karyawan :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(174, 108);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(174, 160);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(100, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = System.Drawing.Color.Gray;
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Location = new System.Drawing.Point(307, 65);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(200, 118);
+            panel1.TabIndex = 5;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new System.Drawing.Point(3, 91);
+            label9.Name = "label9";
+            label9.Size = new System.Drawing.Size(75, 15);
+            label9.TabIndex = 15;
+            label9.Text = "FIN : Finance";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new System.Drawing.Point(3, 76);
+            label8.Name = "label8";
+            label8.Size = new System.Drawing.Size(90, 15);
+            label8.TabIndex = 14;
+            label8.Text = "PM : Product M";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new System.Drawing.Point(3, 61);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(90, 15);
+            label7.TabIndex = 13;
+            label7.Text = "DEV : Developer";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(3, 46);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(85, 15);
+            label6.TabIndex = 12;
+            label6.Text = "ENG : Engineer";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(3, 29);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(48, 15);
+            label5.TabIndex = 11;
+            label5.Text = "HR : HR";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(3, 5);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(90, 15);
+            label4.TabIndex = 10;
+            label4.Text = "lD Departement";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new System.Drawing.Point(29, 273);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new System.Drawing.Size(478, 150);
+            dataGridView1.TabIndex = 6;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new System.Drawing.Point(29, 209);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new System.Drawing.Size(99, 34);
+            btnInsert.TabIndex = 7;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new System.Drawing.Point(227, 209);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new System.Drawing.Size(99, 34);
+            btnEdit.TabIndex = 8;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // btn_Delete
+            // 
+            btn_Delete.Location = new System.Drawing.Point(421, 209);
+            btn_Delete.Name = "btn_Delete";
+            btn_Delete.Size = new System.Drawing.Size(86, 34);
+            btn_Delete.TabIndex = 9;
+            btn_Delete.Text = "Delete";
+            btn_Delete.UseVisualStyleBackColor = true;
+            btn_Delete.Click += btn_Delete_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(btn_Delete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnInsert);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btn_Delete;
+    }
+}
---
+++ responsi 2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace responsi_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new NpgsqlConnection(connstring);
+        }
+
+        private NpgsqlConnection conn;
+        string connstring = "Host=localhost;Port=5432;Username=postgres;Password=informatika;Database=responsijunpro";
+        public DataTable dt;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+        private DataGridView r;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
+
+
